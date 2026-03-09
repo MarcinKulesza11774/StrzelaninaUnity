f655d4fc670b73b224f00685396807826935534e
Marcin Kulesza <11774@kasprzak.edu.pl>
2026-03-09 14:13:38 +0100

    camera changes

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -4,6 +4,8 @@ public class CameraControll : MonoBehaviour
 {
     public Transform player;
     public float mouseSensitivity = 120f;
+    public Vector3 aimingCameraRotation;
+    public Vector3 aimingCameraPosition;
 
     void Start()
     {
@@ -15,5 +17,17 @@ public class CameraControll : MonoBehaviour
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
         player.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            transform.localPosition = transform.localPosition + aimingCameraPosition;
+            transform.Rotate(aimingCameraRotation);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            transform.localPosition = transform.localPosition - aimingCameraPosition;
+            transform.Rotate(-aimingCameraRotation);
+        }
     }
 }
